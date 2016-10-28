6f0fa37ddae499d75c38f5e48bd0815a6929b072
Mateusz Nowak <mateusznowak@vp.pl>
2016-10-28 23:15:23 +0200

    Added helios film load

--- src/main/java/repertuar/service/impl/HeliosService.java
+++ src/main/java/repertuar/service/impl/HeliosService.java
@@ -1,6 +1,8 @@
 package repertuar.service.impl;
 
 import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.DomNode;
+import org.apache.commons.lang3.time.DateUtils;
 import repertuar.model.Cinema;
 import repertuar.model.Film;
 import repertuar.model.SeanceDay;
@@ -9,11 +11,15 @@ import repertuar.service.api.ChainService;
 import repertuar.utils.Website;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class HeliosService implements ChainService {
+
     @Override
     public List<Cinema> getCinemas() throws IOException {
         return new Website("http://helios.pl")
@@ -32,12 +38,29 @@ public class HeliosService implements ChainService {
 
     @Override
     public List<SeanceDay> getSeanceDays(Integer cinemaID) throws IOException {
-        return null;
+        List<SeanceDay> seanceDays = new LinkedList<>();
+
+        for (int i = 0; i < 7; i++) {
+            seanceDays.add(
+                    new SeanceDay(
+                            DateUtils.addDays(new Date(), i),
+                            Collections.emptyList()
+                    )
+            );
+        }
+
+        return seanceDays;
     }
 
     @Override
     public List<Film> getFilms(Integer cinemaID, Date date) throws IOException {
-        return null;
+        return new Website("http://helios.pl/" + cinemaID + "/Repertuar/index/dzien/" + daysDifference(new Date(), date))
+                .loadPageWithJavaScriptDisabled()
+                .getElementsByTagName("a")
+                .stream()
+                .filter(e -> e.hasAttribute("class") && "movie-link".equals(e.getAttribute("class")))
+                .map(this::prepareFilm)
+                .collect(Collectors.toList());
     }
 
     private Cinema prepareCinema(DomElement e) {
@@ -65,4 +88,35 @@ public class HeliosService implements ChainService {
 
         return new HeliosCinema(id, city + name.replaceFirst("Helios", ""), "http://helios.pl" + href);
     }
+
+    private Film prepareFilm(DomElement e) {
+        StringBuilder title = new StringBuilder();
+        StreamSupport
+                .stream(e.getChildren().spliterator(), false)
+                .map(DomNode::getTextContent)
+                .map(String::trim)
+                .map(s -> {
+                    if (s.startsWith("/")) {
+                        s = s.replace('/', ',');
+                    }
+                    return s;
+                })
+                .forEach(title::append);
+        return new Film(title.toString(), "http://helios.pl" + e.getAttribute("href"), Collections.emptyList());
+    }
+
+    private int daysDifference(Date d1, Date d2) {
+        Date earlierDate = d1.before(d2) ? d1 : d2;
+        Date laterDate = d1.before(d2) ? d2 : d1;
+
+        int diff = 0;
+
+        while (!DateUtils.isSameDay(earlierDate, laterDate)) {
+            earlierDate = DateUtils.addDays(earlierDate, 1);
+            diff++;
+        }
+
+        return diff;
+    }
+
 }
