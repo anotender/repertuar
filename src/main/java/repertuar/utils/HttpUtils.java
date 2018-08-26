9d165cd493580d54d00830ebccb2d2d6e2abff81
Mateusz Nowak <mateusznowak@vp.pl>
2018-08-26 17:12:14 +0200

    Cleanup in HttpUtils.java

--- src/main/java/repertuar/utils/HttpUtils.java
+++ src/main/java/repertuar/utils/HttpUtils.java
@@ -6,46 +6,16 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class HttpUtils {
 
-    private static final HttpClient client = HttpClientBuilder.create().build();
+    private static final HttpClient HTTP_CLIENT = HttpClientBuilder.create().build();
 
     public static String sendGet(String url) throws IOException {
-        return sendGet(url, Collections.emptyMap());
-    }
-
-    public static String sendGet(String url, Map<String, String> params) throws IOException {
-        HttpGet getRequest = new HttpGet(prepareGetUrl(url, params));
-        HttpResponse getResponse = client.execute(getRequest);
-
-        String content = new BufferedReader(new InputStreamReader(getResponse.getEntity().getContent(), "UTF-8"))
-                .lines()
-                .collect(Collectors.joining("\n"));
-
-        EntityUtils.consume(getResponse.getEntity());
-
-        return content;
-    }
-
-    private static String prepareGetUrl(String url, Map<String, String> params) {
-        StringBuilder builder = new StringBuilder(url);
-        builder.append("?");
-
-        params.forEach((k, v) -> builder
-                .append(k)
-                .append("=")
-                .append(v)
-                .append("&")
-        );
-
-        return builder.toString();
+        HttpGet getRequest = new HttpGet(url);
+        HttpResponse getResponse = HTTP_CLIENT.execute(getRequest);
+        return EntityUtils.toString(getResponse.getEntity());
     }
 
 }
